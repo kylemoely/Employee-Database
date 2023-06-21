b9fc6b231000b9af1ff4b9c7ca211f8dad36a660
kylemoely <kylemoely@gmail.com>
2023-06-21 12:38:54 -0600

    set up table structure for database

---
+++ db/schema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+CREATE TABLE IF NOT EXISTS department(
+    id INT PRIMARY KEY,
+    name VARCHAR(30)
+);
+
+CREATE TABLE IF NOT EXISTS role(
+    id INT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS employee(
+    id INT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES role(id)
+    ON DELETE SET NULL
+);
