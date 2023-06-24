9e8a7ff1787bd7eac07f40fd365acd956ff45203
kylemoely <kylemoely@gmail.com>
2023-06-23 21:05:35 -0600

    Added the three read only queries

--- index.js
+++ index.js
@@ -25,3 +25,53 @@ const question = {
     ]
 };
 
+const viewAllDepartments = () => {
+    db.query('SELECT name AS department, id FROM department', (err, res) => {
+        if(err){
+            console.log(err);
+        } else{
+            console.table(res);
+        };
+    });
+};
+
+const viewAllroles = () => {
+    db.query(`SELECT role.id, role.title, role.salary, department.name AS department FROM role
+              LEFT JOIN department ON role.department_id = department.id`, (err, res) => {
+        if(err){
+            console.log(err);
+        } else{
+            console.table(res);
+        };
+    });
+};
+
+const viewAllEmployees = () => {
+    db.query(`SELECT employee.id, CONCAT(employee.first_name, " ", employee.last_name) AS name, role.title AS role,
+              department.name AS department, role.salary AS salary
+              FROM employee
+              LEFT JOIN role ON employee.role_id = role.id
+              LEFT JOIN department ON role.department_id = department.id`, (err, res) => {
+                if(err){
+                    console.log(err);
+                } else{
+                    console.table(res);
+                };
+              });
+};
+
+inquirer.prompt(question).then((response) => {
+    switch(response.task){
+        case 'View All Departments':
+            viewAllDepartments();
+            break;
+        case 'View All Roles':
+            viewAllroles();
+            break;
+        case 'View All Employees':
+            viewAllEmployees();
+            break;
+    }
+}).catch((err) => {
+    console.log(err);
+});
