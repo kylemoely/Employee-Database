3eb4a729eee9eba6a1195a26c324f5ab126d53b8
kylemoely <kylemoely@gmail.com>
2023-06-24 23:57:11 -0600

    Added updateEmployee function

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const question = {
     choices: [
         'View All Employees',
         'Add Employee',
-        'Update Employee Role',
+        'Update Employee',
         'View All Roles',
         'Add Role',
         'View All Departments',
@@ -143,7 +143,80 @@ const addEmployee = () => {
     })
 }
 
-inquirer.prompt(question).then((response) => {
+const updateEmployee = () => {
+    inquirer.prompt([
+        {
+            type: 'number',
+            name: 'empId',
+            message: `Enter the employee's id:`
+        },
+        {
+            type: 'list',
+            name: 'editOpt',
+            message: 'What would you like to update?',
+            choices: [
+                `Employee's role`,
+                `Employee's manager`,
+                `Employee's first name`,
+                `Employee's last name`
+            ]
+        }
+    ]).then((answers) => {
+        const updateQuery = (empId, column, value) => {
+            db.query(`UPDATE employee
+                      SET ${column} = ${value} 
+                      WHERE id = ${empId}`, (err) => {
+                        if(err){
+                            console.log(err);
+                        } else{
+                            console.log('Employee updated.');
+                        }
+                      })
+        }
+        switch(answers.editOpt){
+            case `Employee's role`:
+                inquirer.prompt([
+                    {
+                        type: 'number',
+                        name: 'roleId',
+                        message: `Enter the employee's new role id:`
+                    }
+                ]).then((answers2) => updateQuery(answers.empId, 'role_id', answers2.roleId));
+                break;
+            case `Employee's manager`:
+                inquirer.prompt([
+                    {
+                        type: 'number',
+                        name: 'manId',
+                        message: `Enter the employee's new manager id:`,
+                        default: null
+                    }
+                ]).then((answers2) => updateQuery(answers.empId, 'manager_id', answers2.manId));
+                break;
+            case `Employee's first name`:
+                inquirer.prompt([
+                    {
+                        type: 'input',
+                        name: 'empFirst',
+                        message: `Enter the employee's new first name:`
+                    }
+                ]).then((answers2) => updateQuery(answers.empId, 'first_name', `'${answers2.empFirst}'`));
+                break;
+            case `Employee's last name`:
+                inquirer.prompt([
+                    {
+                        type: 'input',
+                        name: 'empLast',
+                        message: `Enter the employee's new last name:`
+                    }
+                ]).then((answers2) => updateQuery(answers.empId, 'last_name', `'${answers2.empLast}'`));
+                break;
+        }
+    })
+}
+
+inquirer.prompt(question)
+.then((response) => {
     switch(response.task){
         case 'View All Departments':
             viewAllDepartments();
@@ -163,7 +236,11 @@ inquirer.prompt(question).then((response) => {
         case 'Add Employee':
             addEmployee();
             break;
+        case 'Update Employee':
+            updateEmployee();
+            break;
     }
-}).catch((err) => {
+})
+.catch((err) => {
     console.log(err);
 });
