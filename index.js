bef7da9cc282640bb491df4f39883f0eb4d287a2
kylemoely <kylemoely@gmail.com>
2023-06-24 22:25:50 -0600

    Fixed viewEmployees function to display manager name

--- index.js
+++ index.js
@@ -47,11 +47,12 @@ const viewAllroles = () => {
 };
 
 const viewAllEmployees = () => {
-    db.query(`SELECT employee.id, CONCAT(employee.first_name, " ", employee.last_name) AS name, role.title AS role,
-              department.name AS department, role.salary AS salary
-              FROM employee
-              LEFT JOIN role ON employee.role_id = role.id
-              LEFT JOIN department ON role.department_id = department.id`, (err, res) => {
+    db.query(`SELECT E1.id, CONCAT(E1.first_name, " ", E1.last_name) AS name, role.title AS role,
+              department.name AS department, role.salary AS salary, CONCAT(E2.first_name, " ", E2.last_name) AS manager
+              FROM employee E1
+              LEFT JOIN role ON E1.role_id = role.id
+              LEFT JOIN department ON role.department_id = department.id
+              LEFT JOIN employee E2 ON E1.manager_id = E2.id`, (err, res) => {
                 if(err){
                     console.log(err);
                 } else{
